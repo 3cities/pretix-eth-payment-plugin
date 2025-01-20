16f6e36dd7356948f907106ed7a85064d858bc16
Andrew <andrewliu08@gmail.com>
2025-01-19 22:41:00 -0800

    fix dep versions

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
     install_requires=[
         "pretix>=4.16",
         "web3>=6",
+        "eth-account<=0.8.0",
+        "setuptools>=68.0.0",
         # django-bootstrap3 22.2 under py3.8, added for pip legacy resolver to avoid conflicts
         'importlib-metadata<3; python_version<"3.8"',
     ],
